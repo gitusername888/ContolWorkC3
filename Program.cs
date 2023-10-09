9b1c69d97a35f5808eb8a008e1b7b5aeef850981
Di <you@example.com>
2023-10-09 20:58:46 +0300

    введем массив

--- Program.cs
+++ Program.cs
@@ -4,3 +4,33 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] InputArray ()
+{
+    System.Console.WriteLine("Введите количество значений массива:");
+    int n = Convert.ToInt32(Console.ReadLine ());
+    
+    string [] arrayStart = new string [n];
+    for (int i= 0; i <n; i++)
+    {
+        System.Console.WriteLine($"Введите {i}-e значение массива:");
+        arrayStart [i] = Console.ReadLine ();
+
+    }
+    return arrayStart;
+}
+
+void PrintArray(string[] arrayStart)
+{
+ 
+    for (int i = 0; i < arrayStart.GetLength(0); i++)
+    {
+        System.Console.Write($"{arrayStart[i]}  ");
+        
+    }
+
+}
+
+string[] arrayStart =  InputArray ();
+PrintArray(arrayStart);
+
